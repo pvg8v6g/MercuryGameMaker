4e8afcb6e53eb2a9954bb8fe5f2f929fe8a56912
philbusiness52@gmail.com <philbusiness52@gmail.com>
2026-04-10 14:39:33 -0500

    c

---
+++ GameMaker/UX/Components/GridArea/CanvasItemsControl.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace GameMaker.UX.Components.GridArea;
+
+public class CanvasItemsControl : ItemsControl
+{
+    public static readonly DependencyProperty LeftPropertyNameProperty = DependencyProperty.Register(
+        nameof(LeftPropertyName), typeof(string), typeof(CanvasItemsControl), new PropertyMetadata(null));
+
+    public string LeftPropertyName
+    {
+        get => (string) GetValue(LeftPropertyNameProperty);
+        set => SetValue(LeftPropertyNameProperty, value);
+    }
+
+    public static readonly DependencyProperty TopPropertyNameProperty = DependencyProperty.Register(
+        nameof(TopPropertyName), typeof(string), typeof(CanvasItemsControl), new PropertyMetadata(null));
+
+    public string TopPropertyName
+    {
+        get => (string) GetValue(TopPropertyNameProperty);
+        set => SetValue(TopPropertyNameProperty, value);
+    }
+
+    protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+    {
+        base.PrepareContainerForItemOverride(element, item);
+
+        if (element is not UIElement container || item is not INotifyPropertyChanged notifier) return;
+
+        ApplyPosition(container, item);
+
+        notifier.PropertyChanged += (_, e) =>
+        {
+            if (e.PropertyName == LeftPropertyName || e.PropertyName == TopPropertyName)
+                ApplyPosition(container, item);
+        };
+    }
+
+    private void ApplyPosition(UIElement container, object item)
+    {
+        var type = item.GetType();
+
+        if (type.GetProperty(LeftPropertyName)?.GetValue(item) is double left)
+            Canvas.SetLeft(container, left);
+        else if (type.GetProperty(LeftPropertyName)?.GetValue(item) is int leftInt)
+            Canvas.SetLeft(container, leftInt);
+
+        if (type.GetProperty(TopPropertyName)?.GetValue(item) is double top)
+            Canvas.SetTop(container, top);
+        else if (type.GetProperty(TopPropertyName)?.GetValue(item) is int topInt)
+            Canvas.SetTop(container, topInt);
+    }
+}
---
+++ GameMaker/UX/Utilities/CanvasBind.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace GameMaker.UX.Utilities;
+
+public static class CanvasBind
+{
+    public static readonly DependencyProperty LeftProperty = DependencyProperty.RegisterAttached(
+        "Left",
+        typeof(double),
+        typeof(CanvasBind),
+        new PropertyMetadata(0d, OnLeftChanged));
+
+    public static void SetLeft(DependencyObject element, double value)
+    {
+        element.SetValue(LeftProperty, value);
+    }
+
+    public static double GetLeft(DependencyObject element)
+    {
+        return (double) element.GetValue(LeftProperty);
+    }
+
+    private static void OnLeftChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is UIElement element)
+        {
+            Canvas.SetLeft(element, (double) e.NewValue);
+        }
+    }
+
+    public static readonly DependencyProperty TopProperty = DependencyProperty.RegisterAttached(
+        "Top",
+        typeof(double),
+        typeof(CanvasBind),
+        new PropertyMetadata(0d, OnTopChanged));
+
+    public static void SetTop(DependencyObject element, double value)
+    {
+        element.SetValue(TopProperty, value);
+    }
+
+    public static double GetTop(DependencyObject element)
+    {
+        return (double) element.GetValue(TopProperty);
+    }
+
+    private static void OnTopChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is UIElement element)
+        {
+            Canvas.SetTop(element, (double) e.NewValue);
+        }
+    }
+}
